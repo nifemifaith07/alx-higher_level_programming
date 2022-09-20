dfae948c7f549ad0b5a229e75bed73d64db62432
nifemifaith07 <nifemifaith07@gmail.com>
2022-09-19 17:33:48 -0700

    concat

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -5,4 +5,4 @@ middle_word = word[1:-1]
 word_first_3 = word[:3]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
-print(f"Middle word: {middle_word}") 
+print(f"Middle word: {middle_word}")
