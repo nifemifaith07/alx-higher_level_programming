71a50f8fe6cb98ee70831ec7aee505fa616c9102
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-09-23 04:22:53 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,17 +2,17 @@
 from sys import argv
 
 
-def options():
-	print("{} argument".format(len(argv) - 1, end='')
-	if len(argv) == 1:
-		  print('s.')
-	elif len(argv) == 2:
-		  print(':')
-	else:
-		  print('s:')
-	for i in range(1, len(argv)):
-		  print('{}: {}'.format(i, argv[i]))
+def principal():
+    print('{} argument'.format(len(argv) - 1), end='')
+    if len(argv) == 1:
+        print('s.')
+    elif len(argv) == 2:
+        print(':')
+    else:
+        print('s:')
+    for i in range(1, len(argv)):
+        print('{}: {}'.format(i, argv[i]))
 
 
 if __name__ == "__main__":
-	options()
+    principal()
