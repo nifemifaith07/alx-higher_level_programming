b095a0df922cf573a7ecb52611b48a5e07ba69da
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-09-24 23:48:12 +0100

    print 🔢🥱

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """
+    Prints all integers of a list(one per line)
+    """
+    for x in range(len(my_list)):
+        print("{:d}".format(my_list[x]))
