00f8dccb0c6f811575fffd073a92f4867c81fecf
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-09-25 00:30:30 +0100

    style😏😫

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
- def element_at(my_list, idx):
-        """
-        retrieves an element from a list like in C.
-        """
-        return(my_list[idx] if 0 <= idx < len(my_list) else None)
+def element_at(my_list, idx):
+    """
+    retrieves an element from a list like in C.
+    """
+    return (my_list[idx] if 0 <= idx < len(my_list) else None)
