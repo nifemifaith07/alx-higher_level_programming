81cd98b52b568d742cf8ba556bba8584b4234cbe
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-09-25 15:12:46 +0100

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,6 +7,6 @@ def new_in_list(my_list, idx, element):
     """
     new_list = my_list[:]
     if 0 <= idx < len(my_list):
-        new_lost[idx] = element
+        new_list[idx] = element
         return (new_list)
     return (my_list)
