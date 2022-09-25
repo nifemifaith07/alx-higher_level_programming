fc4bc2332e12ca342721a3c1d9d8ed34d9e4bf30
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-09-25 15:11:47 +0100

    Create 4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    """
+    replaces an element in a list at a specific position
+    without modifying the original list (like in C).
+    """
+    new_list = my_list[:]
+    if 0 <= idx < len(my_list):
+        new_lost[idx] = element
+        return (new_list)
+    return (my_list)
