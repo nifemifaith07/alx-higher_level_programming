f3a825556b7dc71e68d9dd8c59e93c51ce9e2cdb
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-09-26 14:36:38 +0100

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,6 +8,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     The second element should be the addition of the second element of each argument
     """
     a, b = len(tuple_a), len(tuple_b)
-    tuple_n = ((tuple_a[0] if a >= 1 else 0) + (tuple_b[0] if a >= 1 else 0),
-               (tuple_a[1] if a >= 2 else 0) + (tuple_b[1] if a >= 2 else 0))
+    tuple_n = ((tuple_a[0] if a >= 1 else 0) + (tuple_b[0] if b >= 1 else 0),
+               (tuple_a[1] if a >= 2 else 0) + (tuple_b[1] if b >= 2 else 0))
     return (tuple_n)
