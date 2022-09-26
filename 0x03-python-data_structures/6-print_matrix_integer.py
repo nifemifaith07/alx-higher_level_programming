a35f25dc300392d233be80445446efacd041d1b0
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-09-26 04:21:53 +0100

    style error

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,4 @@ def print_matrix_integer(matrix=[[]]):
     """
     prints a matrix of integers.
     """
-    print('\n'.join([" ".join(["{:d}".format(item) for item in row]) 
-      for row in matrix]))
+    print('\n'.join([" ".join(["{:d}".format(item) for item in row]) for row in matrix]))
