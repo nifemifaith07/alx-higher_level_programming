76ec29d18da63f1fd8a9554fc788db486e002193
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-09-26 14:47:47 +0100

    that wasn't it😔😒

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,8 +4,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
     """
     adds two tuple
     Returns a tuple with 2 integers:
-    The first element should be the addition of the first element of each argument
-    The second element should be the addition of the second element of each argument
+    The first element is the sum of first element of each argument
+    The second element is the sum of second element of each argument
     """
     a, b = len(tuple_a), len(tuple_b)
     tuple_n = ((tuple_a[0] if a >= 1 else 0) +
