fd9353cac9f0bdd66c051ffc587130871d3849f2
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-09-26 04:14:52 +0100

    matrix

---
+++ 0x03-python-data_structures/a.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    """
+    prints a matrix of integers.
+    """
+    print('\n'.join([''.join(['{:d}'.format(item) for item in row]) 
+      for row in matrix]))
