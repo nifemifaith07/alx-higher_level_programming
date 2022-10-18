f6c29675fe9efe85e5993662326553f0a2623f87
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-10-18 11:56:07 +0100

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,9 +2,9 @@
 def roman_to_int(roman_string):
     ri = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     i = 0
-    n = len(roman_string)
     sum = 0
     if isinstance(roman_string, str):
+        n = len(roman_string)
         while i < n:
             if (i != n - 1 and ri[roman_string[i]] < ri[roman_string[i + 1]]):
                 sum -= ri[roman_string[i]]
