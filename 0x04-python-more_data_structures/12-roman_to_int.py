9b5283872e5c3f2a1ce3fe9e6758657f3f5246b7
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-10-18 11:39:16 +0100

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,7 @@ def roman_to_int(roman_string):
     sum = 0
     if isinstance(roman_string, str):
         while i < n:
-            if (i != n - 1 && ri[roman_string[i]] < ri[roman_string[i + 1]]):
+            if (i != n - 1 and ri[roman_string[i]] < ri[roman_string[i + 1]]):
                 sum -= ri[roman_string[i]]
             else:
                 sum += ri[roman_string[i]]
