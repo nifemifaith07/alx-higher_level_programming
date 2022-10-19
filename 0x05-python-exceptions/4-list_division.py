0a814d8a4d8075dcfec61b445580306df05b1170
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-10-19 16:59:21 +0100

    Update 4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    for i in range(list_lenght):
+    for i in range(list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
         except (ValueError, TypeError):
