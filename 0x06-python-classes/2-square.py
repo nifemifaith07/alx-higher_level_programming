874081e983d3d6a2129deaba40a09320f4a088d3
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-10-23 00:10:27 +0100

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-"""
-Defines a class Square and validate the size.
-"""
+"""defines a class Square"""
+
 
 class Square:
-    """Represent a Square
+    """Represents a square
     Attributes:
-        __size (int): size of the side of the square
+        __size (int): size of a side of the square
     """
     def __init__(self, size=0):
-        """Initializes a square
+        """initializes the square
         Args:
-            size (int): size of a side of the Square
-        Returns: 
+            size (int): size of a side of the square
+        Returns:
             None
         """
         if type(size) is not int:
-        raise TypeError("size must be an integer")
+            raise TypeError("size must be an integer")
         else:
             if size < 0:
                 raise ValueError("size must be >= 0")
