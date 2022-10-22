261a1db517f759fa3017de3288965b64beb383b0
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-10-22 22:36:18 +0100

    square with size check

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+sloc)  358 Bytes
+#!/usr/bin/python3
+"""Square class definition"""
+
+
+class Square:
+    """Represent a Square
+    Attributes:
+        __size (int): size of the side of the square
+    """
+    def __init__(self, size=0):
+        """Initializes a square
+        Args:
+            size (int): size of a side of the Square
+        Returns: None
+        """
+        if type(size) is not int:
+        raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
