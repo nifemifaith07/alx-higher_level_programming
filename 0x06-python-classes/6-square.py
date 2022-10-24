aa501f3fc185cc48799e90ab98f048948cd2150b
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-10-24 17:33:24 +0100

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -12,7 +12,7 @@ class Square:
         """initializes the square
         Args:
             size (int): size of a side of the square
-            position (tuple): positoin of the square in 2D space
+            position (tuple): position of the square in 2D space
         Returns:
             None
         """
@@ -81,7 +81,8 @@ class Square:
             None
         """
         if type(value) is not tuple or len(value) != 2 or \
-           type(value[0]) is not int or type(value[1]) is not int:
+           type(value[0]) is not int or type(value[1]) is not int or \
+           value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
