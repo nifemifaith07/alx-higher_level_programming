25d08a50c264a599ad3e770d30ea6394f21d8d0f
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-10-26 13:12:55 +0100

    Create 3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for 3. To JSON string"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """Returns JSON representation of object my_obj"""
+    return (json.dumps(my_obj))
