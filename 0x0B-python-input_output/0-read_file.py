ea3c162c9c59604e3d15f0e18434fb37bdbf40ba
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-10-26 12:35:01 +0100

    Create 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for 0. Read File """
+
+
+def read_file(filename=""):
+    """Reads a file and prints it to STDOUT"""
+
+    with open("filename", "r", encoding="UTF-8") as file:
+        for line in file:
+            print(line, end="")
