ef8f110e6d3b747dc59f122406699f9366b5008c
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-10-26 13:19:25 +0100

    Update 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -2,6 +2,9 @@
 """Module for 4. From JSON string to Object"""
 
 
+inport json
+
+
 def from_json_string(my_str):
     """Returns object represented by JSON string(my_str)"""
     return (json.loads(my_str))
