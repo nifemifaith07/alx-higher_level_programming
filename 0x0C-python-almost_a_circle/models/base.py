bac6bdf18d1da9d4dbfac9a7b890601f93067f6a
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-11-03 10:28:37 +0100

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,27 @@
+#!/usr/bin/python3
+"""
+This module implements `base` class of all other classes in this project.
+The goal of it is to manage id attribute in all your future classes
+and to avoid duplicating the same code (by extension, same bugs)
+"""
+import json
+import turtle
 
+
+class Base:
+    """
+    implementation
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """initialization
+        Args:
+            id (int, optional): object id. Defaults to None.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
