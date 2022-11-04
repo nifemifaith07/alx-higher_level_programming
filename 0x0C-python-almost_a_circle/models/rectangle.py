379ed15d7e1220874b63576c8ab2ddfaf04c52d6
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-11-04 13:49:13 +0100

    Create rectangle.py

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+This module implements a Rectangle object that inherits from Base
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Rectangle implementation"""
+
+    def __init__(self, width: int, height: int, x=0, y=0, id=None):
+        """initialization"""
+        super().__init__(id)
+
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+    
+    @property
+    def width(self) -> int:
+        """width getter"""
+        return self.__width
+    
+    @width.setter
+    def width(self, width: int):
+        """width setter"""
+        self.__width = width
+    
+    @property
+    def height(self) -> int:
+        """height getter"""
+        return self.__height
+    
+    @height.setter
+    def height(self, height: int):
+        """height setter"""
+        self.__height = height
+    
+    @property
+    def x(self) -> int:
+        """x getter"""
+        return self.__x
+    
+    @x.setter
+    def x(self, x: int):
+        """x setter"""
+        self.__x = x
+    
+    @property
+    def y(self) -> int:
+        """y getter"""
+        return self.__y
+    
+    @y.setter
+    def y(self, y: int):
+        """y setter"""
+        self.__y = y
