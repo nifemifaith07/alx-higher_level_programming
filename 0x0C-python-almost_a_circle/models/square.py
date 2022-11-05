8e1092a0264a43dcf7ee1586844544a39f180eb4
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-11-05 07:10:33 +0100

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,20 +11,18 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """initialization"""
         super().__init__(size, size, x, y, id)
-        self.size = size
 
     @property
     def size(self):
         """size getter"""
-        return self.size
+        return self.width
 
     @size.setter
     def size(self, value):
         """size setter"""
         self.width = self.height = value
-        self.size = value
 
     def __str__(self):
         """overides the str method"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
-                                                 self.y, self.size)
+                                                 self.y, self.width)
