e3e0ec74877a4a63cc180d9a44e04d2dde5bab71
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-11-07 20:45:30 +0100

    Update test_square.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -12,7 +12,7 @@ class TestSquare(unittest.TestCase):
     def test_initialization_success(self):
         s1 = Square(5)
         s2 = Square(10)
-        self.assertEqual(s1.id, 5)
+        self.assertEqual(s1.id, 2)
         self.assertEqual(s2.id, 6)
 
     def test_initialization_without_arguments(self):
