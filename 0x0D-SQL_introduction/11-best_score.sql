063b592f3d1f09b99c466ceecfe4ef10cadb1c0d
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-12-01 23:36:46 +0100

    Create 11-best_score.sql

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,8 @@
+-- lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0
+-- Results should display both the score and the name (in this order)
+-- Records should be ordered by score (top first)
+-- The database name will be passed as an argument of the mysql command
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDEE BY score DESC;
