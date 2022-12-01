835d5acb9ad1d189d4b8bdb6d062b6b2f4e810a1
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-12-01 23:27:38 +0100

    Create 9-full_creation.sql

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- creates a table second_table in the database hbtn_0c_0
+-- second_table description:id INT, name VARCHAR(256), score INT
+-- script should not fail, if table exists already
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+ 
+-- script should create four records
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
