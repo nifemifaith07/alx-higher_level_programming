7e54ff5ee0d5508ece4df28e0b8fd23b74cc752c
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-12-03 00:57:42 +0100

    Update 102-top_city.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,9 +1,8 @@
 -- Using temperature data set from ex.18
 -- Display top 3 cities by temperature during July and August
 -- ordered by temperature descending
--- This throws an error if ONLY_FULL_GROUP_BY is enabled. This disable this,
--- run: SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
-SELECT city, month, AVG(value) AS avg_temp FROM temperatures
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
 WHERE (month=7 OR month=8)
 GROUP BY city
 ORDER BY avg_temp DESC
