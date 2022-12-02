fd2fc71920bd50310d2b38d0b00c5a9e3debfe1c
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-12-02 22:06:34 +0100

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,9 +1,8 @@
 -- converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
--- You need to convert all of the following to UTF8:
 -- convert database hbtn_0c_0
 ALTER DATABASE hbtn_0c_0
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
 
 -- Convert Table first_table
 USE hbtn_0c_0;
