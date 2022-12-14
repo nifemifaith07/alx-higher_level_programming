5a14b1cd54e1c32057ca301ae7fbfd750037eaf5
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-12-14 11:46:44 +0100

    Create 7-cities.sql

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,18 @@
+-- create the database hbtn_0d_usa
+-- If the database hbtn_0d_usa already exists, your script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- craete the table cities (in the database hbtn_0d_usa) with these descriptions:
+-- id INT unique, auto generated, can’t be null and is a primary key
+-- state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
+-- name VARCHAR(256) can’t be null
+-- If the table cities already exists, your script should not fail
+CRAETE TABLE IF NOT EXISTS hbtn_0d_usa.cities
+(
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (state_id)
+		REFERENCES hbtn_0d_usa.states(id)
+);
