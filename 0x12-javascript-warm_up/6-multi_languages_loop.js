a4889da709ecf80b0a1a853fd44f6e94caa76dda
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-12-27 02:39:13 +0100

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const mssg = ['C is fun', 'Python is
-cool', 'JavaScript is amazing'];
+const mssg = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < 3; i++) {
   console.log(mssg[i]);
 }
