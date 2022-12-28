1439c099d0e3fad402aa10257be4bf626449e5c2
nifemifaith07 <nifemifaith07@gmail.com>
2022-12-28 03:19:56 +0000

    ex

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@ const num = Math.floor(Number(process.argv[2]));
 if (isNaN(num)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i > num; i++) {
+  for (let i = 0; i < num; i++) {
     console.log('C is fun');
   }
 }
