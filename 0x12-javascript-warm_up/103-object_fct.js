1d550923ed8516087e267dc09850eea91345720f
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2023-06-13 16:59:29 +0100

    Update 103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -9,10 +9,10 @@ console.log(myObject);
 i used this.value instead of object.value to make it more versatile 
 and reusable for any other object the method is called on 
 */
-function incr() {
+
+myObject.incr = function () {
   this.value++;
-}
-myObject.incr = incr;
+};
 
 myObject.incr();
 console.log(myObject);
