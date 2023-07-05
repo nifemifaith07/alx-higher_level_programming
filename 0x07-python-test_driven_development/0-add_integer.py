d7c28ab0b73ba6d8061f20b9470a19260f8e3484
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2023-07-05 14:30:35 +0100

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,1 +1,16 @@
+#!/usr/bin/python3
+"""
+This is the '0-add_integer.py' module
+It has a function 'add_integer' with two parameters 'a and b'
+"""
 
+def add_integer(a, b):
+  """ this function adds 2 integers."""
+  if type(a) is not int and type(a) is not float:
+    raise TypeError("a must be an integer")
+  if type(b) is not int and type(b) is not float:
+    raise TypeError("b must be an integer")
+  if type(a) is float:
+    a = int(a)
+  if type(b) is float:
+    b = int(b)
