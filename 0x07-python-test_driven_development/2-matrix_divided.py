28aac0248cf6a15f0e964060ee566183d876be67
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2023-07-07 12:14:27 +0100

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -27,4 +27,4 @@ integers/floats")
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    return [[round(i / div, 2) for i in l] for l in matrix]
+    return [[round(i / div, 2) for i in lt] for lt in matrix]
