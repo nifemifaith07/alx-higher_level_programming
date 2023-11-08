50f42573eba7fa9a5280ebc91ef1133aeae7d9e6
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2023-11-08 17:10:26 +0100

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,16 +6,16 @@ it has one funtion and recieves two parameters
 
 
 def add_integer(a, b=98):
-  """
-  Function adds two integers together
-  Args
+    """
+    Function adds two integers together
+    Args
     a: int or float
     b: int or float
-  """
-  
-  if type(a) is not int and type(a) is not float:
-      raise TypeError("a must be an integer")
-  if type(b) is not int and type(b) is not float:
-      raise TypeError("b must be an integer")
-  
-  return int(a) + int(b)
+    """
+
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
