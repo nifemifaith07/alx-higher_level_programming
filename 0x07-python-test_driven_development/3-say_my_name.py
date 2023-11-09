4e7de6c34cff6c3f9fac75ae97aa993c8ced1091
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2023-11-09 15:10:55 +0100

    Create 3-say_my_name.py

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+!/usr/bin/python3
+"""
+this is the "3-say_my_name" module
+the module has one function "say_my_name
+""""
+
+
+def say_my_name(first_name, last_name=""):
+	"""
+ 	prints 'my name is' followed by first name and optional last name
+ 	first name and last name are strings
+  	"""
+
+wtype(first_name) is not str:
+		raise TypeError("first_name must be a string")
+
+	if type(last_name) is not str:
+		raise TypeError("last_name must be a string")
+
+	print("My name is", first_name, last_name)
