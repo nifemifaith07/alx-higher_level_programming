2c3de8b0d03e219a68d7470f625d719d8327d12b
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2023-11-09 15:34:15 +0100

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -6,15 +6,14 @@ the module has one function "say_my_name
 
 
 def say_my_name(first_name, last_name=""):
-	"""
- 	prints 'my name is' followed by first name and optional last name
- 	first name and last name must be strings
-  	"""
-
-	if type(first_name) is not str:
-		raise TypeError("first_name must be a string")
-
-	if type(last_name) is not str:
-		raise TypeError("last_name must be a string")
-
-	print(f'My name is {first_name} {last_name}')
+    """
+    prints 'my name is' followed by first name and optional last name
+    first name and last name must be strings
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+        
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+        
+    print(f'My name is {first_name} {last_name}')
