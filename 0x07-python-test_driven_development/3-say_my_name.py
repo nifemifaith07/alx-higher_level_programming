6ca09214d28a9054050d1cf1443a457b064535c6
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2023-11-09 15:35:44 +0100

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -12,8 +12,8 @@ def say_my_name(first_name, last_name=""):
     """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-        
+
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-        
+  
     print(f'My name is {first_name} {last_name}')
