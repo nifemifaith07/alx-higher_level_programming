e9cb1e11a5d1f649b792398cd96eba1d2db19beb
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2023-11-28 21:35:36 +0100

    Update 8-rectangle.py

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -14,8 +14,14 @@ class Rectangle:
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """returns the biggest rectangle by area"""
-        
-    
+        if type(rect_1) is not Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) is not Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
+
     def __init__(self, width=0, height=0):
         """initialize a rectangle"""
         self.width = width
