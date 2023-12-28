77de0a31825e84165fb8c5d4909a4a9b37ff9c6e
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2023-12-28 17:11:59 +0100

    Update 16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
--- lists all records of the table second_table of the database hbtn_0c_0
+-- lists all records of the table 'second_table' of the database 'hbtn_0c_0'
 -- Don’t list rows without a name value
 -- Results should display the score and the name (in this order)
 -- Records should be listed by descending score
