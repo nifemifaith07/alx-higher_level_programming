74ccd3933f39cc75c025108c97f9ce500e80691e
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2023-12-28 16:55:34 +0100

    Update 10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
 -- lists all records of the table second_table of the database hbtn_0c_0
--- Results should display both the score and the name (in this order)
+-- Results should display both the score and the name
 -- Records should be ordered by score (top first)
 -- The database name will be passed as an argument of the mysql command
 SELECT score, name
