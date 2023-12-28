cd75518484e72ab3d360be06e1d21152e99f6452
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2023-12-28 17:22:08 +0100

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -9,9 +9,3 @@ USE hbtn_0c_0;
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
-
--- Field name in first_table
--- USE hbtn_0c_0;
--- ALTER TABLE first_table
--- CHANGE name name VARCHAR(256)
--- COLLATE utf8mb4_unicode_ci;
