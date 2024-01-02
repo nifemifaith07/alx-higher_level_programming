3fd0d8c68be7a901f23b932c87c250fafa1db614
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2024-01-02 17:38:57 +0100

    Update 8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 --  lists all the cities of California that can be found in the database hbtn_0d_usa.
--- The states table contains only one record where name = California (but the id can be different, as per the example)
+-- states table contains only one record where name = California (but the id can be different)
 -- Results must be sorted in ascending order by cities.id
 SELECT id, name
 FROM cities
