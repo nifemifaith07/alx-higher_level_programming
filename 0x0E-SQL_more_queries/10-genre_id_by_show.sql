983a0203693363d0cd5d26147514d7076e703b4e
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2024-01-02 17:54:34 +0100

    Update 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,8 @@
---  lists all cities contained in the database hbtn_0d_usa.
--- Each record should display: cities.id - cities.name - states.name
--- Results must be sorted in ascending order by cities.id
+-- Import the database dump from hbtn_0d_tvshows
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- You can use only one SELECT statement
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
