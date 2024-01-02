b6eb0987691efd4878ecb3192567d51c2d686b4e
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2024-01-02 17:15:31 +0100

    Update 6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,11 +1,9 @@
--- create the database hbtn_0d_usa
--- If the database hbtn_0d_usa already exists, your script should not fail
+-- creates database hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
--- create the table states (in the database hbtn_0d_usa) with description:
+-- creates table states (in the database hbtn_0d_usa) with description:
 -- id INT unique, auto generated, can’t be null and is a primary key
 -- name VARCHAR(256) can’t be null
--- If the table states already exists, your script should not fail
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
 (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL,
