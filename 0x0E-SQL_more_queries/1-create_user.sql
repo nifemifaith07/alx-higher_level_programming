d0378a5bcfe1662a39e0791d26ff5459cbe2b0b4
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2024-01-02 16:12:27 +0100

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Create user 'user_0d_1' with all privileges
--- Password for user set as 'user_0d_1_pwd'
+-- Password for user should be set as 'user_0d_1_pwd'
 -- If user already exists, script should not fail
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
