4a0bdebba2e75034c5e82e15aee372ce6d6a5f75
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2024-01-02 16:11:43 +0100

    Update 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- List all privileges for users 'user_0d_1' and 'user_0d_2'
+-- Lists all privileges for users 'user_0d_1' and 'user_0d_2' on your server
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
