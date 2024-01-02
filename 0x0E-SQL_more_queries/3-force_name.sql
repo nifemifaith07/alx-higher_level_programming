a2127ce0d5d81e9e52599a1627127cbd97338414
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2024-01-02 17:04:28 +0100

    Update 3-force_name.sql

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,3 @@
--- creates table force_name with the following description
+-- creates table 'force_name' with the description
 -- id INT, name VARCHAR(256) can’t be null
--- If force_name already exists, your script should not fail
 CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
