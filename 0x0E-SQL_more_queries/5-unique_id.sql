4ddfd095e362642f1b31c0b3f12725d6a20c4bca
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2024-01-02 17:09:37 +0100

    Update 5-unique_id.sql

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,8 +1,7 @@
--- creates the table unique_id with the following description:
--- id INT with the default value 1 and must be unique, name VARCHAR(256)
--- If the table unique_id already exists, your script should not fail
+-- creates table unique_id with the description:
+-- id INT with the default value 1 and unique, name VARCHAR(256)
 CREATE TABLE IF NOT EXISTS unique_id
 (
 	id INT DEFAULT 1 UNIQUE,
 	name VARCHAR(256)
-)	
+)
