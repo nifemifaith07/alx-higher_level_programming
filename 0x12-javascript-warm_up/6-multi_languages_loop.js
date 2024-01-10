ad8e561c5f0c82e30bd76684d2bb5151242974ef
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2024-01-10 13:03:31 +0200

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+// prints 3 lines using an array of string and a loop
 const mssg = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < 3; i++) {
   console.log(mssg[i]);
