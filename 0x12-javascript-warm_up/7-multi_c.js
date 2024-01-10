9b5125670706b5142cbb607ebe881fb9ed043be7
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2024-01-10 13:06:25 +0200

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+// prints x times “C is fun”, x is the 1st argument of the script
 const num = Math.floor(Number(process.argv[2]));
 if (isNaN(num)) {
   console.log('Missing number of occurrences');
