cd87f77d91f117414823616c712fb11406621095
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2024-01-10 12:47:45 +0200

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-const count = process.argv[2];
-if (isNaN(count) === true) {
+// prints "My number: <first argument converted in integer>"
+const numb = process.argv[2];
+if (isNaN(numb) === true) {
   console.log('Not a number');
 } else {
-  console.log('My number: ' + count);
+  console.log('My number: ' + numb)
 }
