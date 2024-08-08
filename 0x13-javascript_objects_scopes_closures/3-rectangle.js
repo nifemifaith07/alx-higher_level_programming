1b1282562d155ee618c5afa60be36bde246a6a94
nifemifaith07 <nifemifaith07@gmail.com>
2024-08-08 16:13:49 +0100

    rectangles

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// A class Rectangle that defines a rectangle
+// Has an instance method called print that prints the rectangle using the character X
+
+class Rectangle {
+  constructor(w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0, i < this.height, i++) {
+      let model = "";
+      for (let j = 0, j < this.width, j++) {
+        model += 'X';
+      }
+    console.log(model);
+    }
+  }
+}
+module.exports = Rectangle;
