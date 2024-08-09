614a40c950ea98d831c35fb800d16493ea3262c6
nifemifaith07 <nifemifaith07@gmail.com>
2024-08-09 19:08:06 +0100

    fix

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 exports.esrever = function (list) {
   const revList = [];
   for (let i = list.length - 1; i >= 0; i--) {
-    revList.push(list[1]);
+    revList.push(list[i]);
   }
 
   return revList;
