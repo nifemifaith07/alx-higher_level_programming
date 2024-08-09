632ef82891cba504efc6416967efb7876f49315d
nifemifaith07 <nifemifaith07@gmail.com>
2024-08-09 12:19:16 +0100

    quick fix

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,7 @@
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
 
-  for (let i = 0; i < list.lenght; i++) {
+  for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
       count++;
     }
