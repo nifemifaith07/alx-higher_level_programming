118a04ae6870fbec820486dbf5ff23c682d46477
nifemifaith07 <nifemifaith07@gmail.com>
2024-08-10 02:40:03 +0100

    quick fix

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+const newDict = {};
+for (const key in dict) {
+  const item = dict(key);
+
+  if (!newDict[item]) {
+    newDict[item] = [];
+  }
+  newDict[item].push(key);
+}
